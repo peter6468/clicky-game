a541e8a976a262445cbae0f1f7c3c8ce01c1f1f3
peter6468 <plimbo@gmail.com>
2018-09-25 23:33:07 -0400

    rough template 0 works

--- game/src/App.js
+++ game/src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import FriendCard from './Components/FriendCard';
+import Nav from './Components/Nav';
+import Title from './Components/Title';
+import Wrapper from './Components/Wrapper';
+
+
+
+
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper> 
+        <Nav
+          tile="Clicky Game"
+          //score = {this.state.currentScore}
+          //topScore= {this.state.topScore}
+          //rightWrong= {this.state.rightWrong}
+          />
+
+          <Title>Click on an image to earn points, but do not click on any more than once!!!</Title>
+      </Wrapper>
+      
     );
   }
 }
